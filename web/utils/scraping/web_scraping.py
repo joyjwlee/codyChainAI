b6a75bfc1b416e631b2326527911399fd41fed1d
Jaewon Lee <joyjwlee@gmail.com>
2023-02-25 15:10:12 -0800

    finished generate_category_links

--- web/utils/scraping/web_scraping.py
+++ web/utils/scraping/web_scraping.py
@@ -1,26 +1,35 @@
 # run this python file to update the json's whenever you want :)
+
+# imports
 from bs4 import BeautifulSoup
 import requests
 import json
 
+# variables
 ao3_domain = "https://archiveofourown.org"
-#TODO: what's the fandoms.json path? and also explore the structure! 
-JSON_PATH = "put path of json here"
+JSON_PATH = "web/json/fandoms.json"
 
-#TODO:? a potential helper function, optional to fill out
 # returns an array of each cateogory link (on which multiple fandoms are listed)
 def generate_category_links():
-    return
+    ao3 = requests.get(ao3_domain)
+    soup = BeautifulSoup(ao3.text)
+    all_categories = soup.select("div.browse a")
+    for i in range(len(all_categories)):
+        all_categories[i] = ao3_domain + str(all_categories[i].attrs["href"])
+    return all_categories
 
 #TODO:? a potential helper function, optional! 
 # returns an array of {"name":"fandom_name", "link":"fandom_link"} for all fandoms
 def get_all_fandoms():
-    return
+    return None
 
 #TODO:? a potential helper function, optional! 
 # returns an array of {"name":"fandom_name", "link":"fandom_link"} for the top most written fandoms in each category
-def get_top_fandoms():  
-   return 
+
+def get_top_fandoms(n):
+    for i in range(n):
+        print(i)
+    return None
 
 #TODO: Week One deliverable ! it's to write a function that will populate fandoms.json 
 # creates fandoms.json file in the json folder with all fandoms and top fandoms in the listed format: '*shoudln't return anything'
