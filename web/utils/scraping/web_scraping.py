7a4801eaf2b17d0bcebbe368be6b70175bca6467
Jaewon Lee <joyjwlee@gmail.com>
2023-02-25 16:27:32 -0800

    finish get_top_fandoms

--- web/utils/scraping/web_scraping.py
+++ web/utils/scraping/web_scraping.py
@@ -4,6 +4,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import re
 
 # variables
 ao3_domain = "https://archiveofourown.org"
@@ -11,27 +12,44 @@ JSON_PATH = "web/json/fandoms.json"
 
 # returns an array of each cateogory link (on which multiple fandoms are listed)
 def generate_category_links():
+    # get site text
     ao3 = requests.get(ao3_domain)
     soup = BeautifulSoup(ao3.text)
+
+    # get all the category divs
     all_categories = soup.select("div.browse a")
+
+    # loop through, concatenate, and return
     for i in range(len(all_categories)):
         all_categories[i] = ao3_domain + str(all_categories[i].attrs["href"])
     return all_categories
 
-#TODO:? a potential helper function, optional! 
+# TODO:? a potential helper function, optional!
 # returns an array of {"name":"fandom_name", "link":"fandom_link"} for all fandoms
 def get_all_fandoms():
+    # all_fandoms_domain = generate_category_links()[0]
+    # print(all_fandoms_domain)
     return None
 
-#TODO:? a potential helper function, optional! 
 # returns an array of {"name":"fandom_name", "link":"fandom_link"} for the top most written fandoms in each category
+def get_top_fandoms():
+    # get site text
+    all_fandoms_domain = generate_category_links()[0]
+    soup = BeautifulSoup(requests.get(all_fandoms_domain).text)
 
-def get_top_fandoms(n):
-    for i in range(n):
-        print(i)
-    return None
+    # get all of the fanfic divs
+    all_top = soup.select("ol.group a")
 
-#TODO: Week One deliverable ! it's to write a function that will populate fandoms.json 
+    # loop through and return
+    dict = {}
+    for i in all_top:
+        div = str(i)
+        name = re.search(">(.*)<", div).group(1)
+        link = ao3_domain + re.search("href=\"(.*)\"", div).group(1)
+        dict[name] = link
+    return dict
+
+# TODO: Week One deliverable ! it's to write a function that will populate fandoms.json
 # creates fandoms.json file in the json folder with all fandoms and top fandoms in the listed format: '*shoudln't return anything'
 # {
 #    "top":[
@@ -48,8 +66,9 @@ def get_top_fandoms(n):
 #    ]
 # }
 def gen_fandom_json():
-    return 
-
+    return
 
 
 # gen_fandom_json() # <-- uncomment this and run the file to update or create fandoms.json
+
+print(get_top_fandoms())
